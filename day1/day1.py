215c9f06a07999e6c47345a7976c8a74e5e6b66b
QuiteChridi <99613353+QuiteChridi@users.noreply.github.com>
2023-12-02 12:08:53 +0100

    Day 1

---
+++ day1/day1.py
@@ -0,0 +1,28 @@
+import re
+
+
+def parseStringToInt(string):
+    strings = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    for i in range(0, 9):
+        if string == strings[i]:
+            return str(i+1)
+    return string
+
+
+# task 1
+sum = 0
+for line in open("day1/input", "r"):
+    numbers = re.findall(r"\d", line)
+    sum += int(numbers[0] + numbers[-1])
+print(sum)
+
+# task 2
+regex = r"(?=(\d|one|two|three|four|five|six|seven|eight|nine))"
+sum = 0
+for line in open("day1/input", "r"):
+    numbers = re.findall(regex, line)
+    print(numbers)
+    numbers = list(map(parseStringToInt, numbers))
+    print(numbers)
+    sum += int(numbers[0] + numbers[-1])
+print(sum)
